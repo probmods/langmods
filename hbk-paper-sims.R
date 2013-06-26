65265a12e00ded8edb0f263e51d9d39c4f0c48b0
Dan Lassiter <danlassiter@stanford.edu>
2013-06-26 15:15:14 -0700

    h

--- hbk-paper-sims.R
+++ hbk-paper-sims.R
@@ -90,6 +90,31 @@ lines(density(bob.str.given.jim.weakest), col='blue', lty=3, lwd=3)
 legend('topright', c("After utterances 1-3", 'After utterances 1-4', 'After utterances 1-5'), lty=c(1, 2, 3), lwd=3, cex=.9, col=c('black', 'red', 'blue'), text.col=c('black', 'red', 'blue'))
 
 # quantifier scope ambiguity
-
-
-
+n.sims = 10000
+num.matches = 4
+num.players = 12
+results = matrix(0, nrow=(num.players/2), ncol=2, dimnames=list(paste("team.size=", 1:(num.players/2), sep=''), c("S-wide", "O-wide")))
+for (team.size in 2:(num.players/2)) {
+  num.accepted = 0
+  while (num.accepted < n.sims) {
+    reading = sample(c("S-wide", "O-wide"), 1)
+    if (reading == "S-wide") {
+      num.teams = (num.players - (num.players %% team.size)) / team.size
+      players.random = sample(1:12, 12, replace=F)
+      matches = unique(as.vector(sapply(1:num.matches, FUN=function(i) {return(sample(1:num.teams, 2, replace=F))})))
+      played = c()
+      for (i in 1:length(matches)) {
+        played = c(played, players.random[matches[i]:(matches[i]+team.size-1)])
+      }
+      if (length(played) == num.players) { 
+  # TCs of subject wide scope reading of "every player played in some match"
+        num.accepted = num.accepted + 1
+        results[team.size, 1] = results[team.size, 1] + 1
+      }
+    } else if (reading == "O-wide" && team.size*2 == num.players) {
+      # TCs of object wide scope reading of "every player played in some match"
+        num.accepted = num.accepted + 1
+        results[team.size, 2] = results[team.size, 2] + 1
+    } 
+  }
+}
