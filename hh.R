5c1065d68064ffbffd8c66d63f011cbfa054c156
Dan Lassiter <danlassiter@stanford.edu>
2013-06-27 15:15:53 -0700

    added simulation of L0 adjective interpretation and surrounding text

--- hh.R
+++ hh.R
@@ -43,11 +43,11 @@ sim.fixing.n.matches = function(n.sims) {
       players.per.team = sample(1:6, 1)
     # from 2 teams to as many teams as possible given team size constraints
       players.in.matches = sapply(1:matches.played, FUN=function(i){return(sample(1:num.players, 2*players.per.team, replace=F))})
-      sws.true = length(unique(as.vector(players.in.matches))) == num.players
-    #  sws.true = length(unique(as.vector(players.in.matches))) > (num.players/2)
+    #  sws.true = length(unique(as.vector(players.in.matches))) == num.players
+      sws.true = length(unique(as.vector(players.in.matches))) > (num.players/2)
     
-      ows.true = some(apply(players.in.matches, FUN=function(v){return(length(v) == num.players)}, MARGIN=2))
-    #  ows.true = some(apply(players.in.matches, FUN=function(v){return(length(v) > num.players/2)}, MARGIN=2))
+    #  ows.true = some(apply(players.in.matches, FUN=function(v){return(length(v) == num.players)}, MARGIN=2))
+      ows.true = some(apply(players.in.matches, FUN=function(v){return(length(v) > num.players/2)}, MARGIN=2))
       if (reading == 's' && sws.true) {
         num.accepted = num.accepted + 1
         results[counter + num.accepted,] = c('s', players.per.team, matches.played)
@@ -77,16 +77,17 @@ for (i in 1:10) {
   sws.team.size[i] = mean(as.numeric(varying.matches[which(varying.matches[,3]==i & varying.matches[,1]=='s'),2]))
   ows.team.size[i] = mean(as.numeric(varying.matches[which(varying.matches[,3]==i & varying.matches[,1]=='o'),2]))
 }
-plot(subj, type='l', col='red', ylim=c(0,1), xlab="Number of matches", ylab="Probability of reading", lwd=3, xaxt='n', lty=2)
+par(mfrow=c(1,2))
+plot(subj, type='l', col='red', ylim=c(.2,.7), xlab="Number of matches", ylab="Probability of reading", lwd=3, xaxt='n', lty=2)
 axis(1, 1:10)
-lines(obj, type='b', col='blue', lty=3, lwd=3)
-legend('bottomleft', c("Subject wide scope", "Object wide scope"), text.col=c('red', 'blue'), lty=c(2,3), col=c('red', 'blue'))
+lines(obj, type='l', col='blue', lty=3, lwd=3)
+legend('bottomleft', c("Subject wide scope", "Object wide scope"), text.col=c('red', 'blue'), lty=c(2,3), col=c('red', 'blue'), cex=.7)
 
-plot(team.size, xaxt='n', col='black', ylim=c(1,6), xlab="Number of matches", ylab="Mean estimated team size", type='l')
-lines(sws.team.size, col='red', lty=2)
-lines(ows.team.size, col='blue', lty=3)
+plot(team.size, xaxt='n', col='black', ylim=c(2,6), xlab="Number of matches", ylab="Mean estimated team size", type='l', lwd=3)
 axis(1, 1:10)
-legend('bottomleft', c("Marginal", "With subject wide scope", "With object wide scope"), text.col=c('black', 'red', 'blue'), lty=c(1,2,3), col=c('black', 'red', 'blue'))
+lines(sws.team.size, col='red', lty=2, lwd=3)
+lines(ows.team.size, col='blue', lty=3, lwd=3)
+legend('bottomleft', c("Marginal", "With subject wide scope", "With object wide scope"), text.col=c('black', 'red', 'blue'), lty=c(1,2,3), col=c('black', 'red', 'blue'), cex=.7)
 
 
 
